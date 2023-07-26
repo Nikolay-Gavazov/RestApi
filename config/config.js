a64a1a03a083b58fec5be7375297b420e57d1f90
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-26 18:50:29 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || 3000,
+        port: 3000,
         dbURL: 'mongodb+srv://Nikeca1991:Zer0mancer@cluster0.f8lwx2u.mongodb.net/',
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
-        port: process.env.PORT || 3000,
-        dbURL: process.env.DB_URL_CREDENTIALS,
+        port: 3000,
+        dbURL: 'mongodb+srv://Nikeca1991:Zer0mancer@cluster0.f8lwx2u.mongodb.net/',
         origin: []
     }
 };
