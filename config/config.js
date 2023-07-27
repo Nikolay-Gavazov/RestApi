39f631f83e0188f41ec2d7fc67820686f232d9b7
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-27 20:12:21 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
     development: {
         port: 3000,
         dbURL: 'mongodb+srv://Nikeca1991:Zer0mancer@cluster0.f8lwx2u.mongodb.net/',
-        origin: []
+        origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
         port: 3000,
