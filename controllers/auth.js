7f7e3754d08c3130b477451c126d1228236639b8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-28 16:13:30 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,7 +58,7 @@ function login(req, res, next) {
             user = removePassword(user);
 
             const token = utils.jwt.createToken({ id: user._id });
-                res.cookie(authCookieName, token, { httpOnly: true, secure: true, sameSite: "true" })
+                res.cookie(authCookieName, token, { httpOnly: true, secure: true,})
            
             res.status(200)
                 .send(user);
