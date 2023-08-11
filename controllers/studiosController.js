84cc819b2efd7eca93c93d4850870ddcc3d9297a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-12 00:36:05 +0300

    Update studiosController.js

--- controllers/studiosController.js
+++ controllers/studiosController.js
@@ -17,7 +17,7 @@ function getStudio(req, res, next) {
 function editStudio(req, res, next) {
     const { id } = req.params;
     const { studioName, img, description } = req.body;
-    studioModel.findOneAndUpdate(id, { studioName, img, description })
+    studioModel.findByIdAndUpdate(id, { studioName, img, description })
         .then(studio => res.json(studio))
         .catch(next);
 }
@@ -30,7 +30,7 @@ function createStudio(req, res, next) {
 function deleteStudio(req, res, next) {
     const { id } = req.params;
 
-    studioModel.findOneAndDelete(id)
+    studioModel.findByIdAndDelete(id)
         .then(studio => res.json(studio))
         .catch(next);
 }
