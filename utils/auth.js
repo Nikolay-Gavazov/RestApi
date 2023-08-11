dfa1c6170aea4fb8ca12874ca0c3e0ae75bfc203
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-11 20:45:13 +0300

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -8,7 +8,7 @@ const {
 function auth(redirectUnauthenticated = true) {
 
     return function (req, res, next) {
-        const token = req.cookies[authCookieName] || '';
+        const token = req.headers("X-Authorization") || '';
         Promise.all([
             jwt.verifyToken(token),
             tokenBlacklistModel.findOne({ token })
